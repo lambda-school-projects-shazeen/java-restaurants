5d5eda526c38da0bf294224264c2160ab9f501c8
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-24 09:42:24 -0700

    setting up test

---
+++ secured-restaurants-initial/src/test/java/com/lambdaschool/restaurants/services/RestaurantServiceImplUnitTestNoDB.java
@@ -0,0 +1,66 @@
+package com.lambdaschool.restaurants.services;
+
+import com.lambdaschool.restaurants.RestaurantsApplicationTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RestaurantsApplicationTests.class)
+public class RestaurantServiceImplUnitTestNoDB
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void findRestaurantByNameLike()
+    {
+    }
+
+    @Test
+    public void findNameCity()
+    {
+    }
+
+    @Test
+    public void findAll()
+    {
+    }
+
+    @Test
+    public void findRestaurantById()
+    {
+    }
+
+    @Test
+    public void findRestaurantByName()
+    {
+    }
+
+    @Test
+    public void delete()
+    {
+    }
+
+    @Test
+    public void save()
+    {
+    }
+
+    @Test
+    public void update()
+    {
+    }
+}
